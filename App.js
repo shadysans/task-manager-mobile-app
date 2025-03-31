5e942d9b11a4304a542b23562297acfc7f797980
shadysans <142474537+shadysans@users.noreply.github.com>
2025-03-31 19:56:40 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import Route from "./src/routes/route";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Route />
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
